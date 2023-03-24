a464ccbdc3ffe813aab9b4a635189e6dba0cd55c
taushif07 <taushifansari1998@gmail.com>
2023-03-24 17:18:04 +0530

    added js test files

---
+++ Month-1/Week-3/day-1/me/index.js
@@ -0,0 +1,37 @@
+// Given the existing code below, can you write some code so that their values are switched around?
+
+// var a = "3";
+// var b = "8";
+// So that the variable a holds the value "8".
+
+// And the variable b holds the value "3".
+
+// When the code is run, it should output:
+
+// a is 8
+
+// b is 3
+
+// Do NOT change any of the existing code.
+
+// You are NOT allowed to type any numbers.
+
+// You should NOT redeclare the variables a and b.
+
+
+function test() {
+    var a = "3";
+    var b = "8";
+    
+/***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+ var x = a;
+     a = b;
+     b = x;
+
+    
+/***********Do not change the code below 👇*******/
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
